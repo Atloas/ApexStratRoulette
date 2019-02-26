4e1af72739fc3fd4221f5d78af622fc288f18f25
Atloas <miter010@gmail.com>
2019-02-26 12:07:23 +0100

    ui

--- Roll.java
+++ Roll.java
@@ -1,8 +1,15 @@
+import java.io.*;
 import java.util.Random;
 import java.util.Vector;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.List;
+import java.util.Arrays;
 
 public class Roll
 {
+    enum weaponRollMode {AMMO, TYPES, WEAPONS};
+
     public static String one(Vector<String> arg)
     {
         Random random = new Random();
@@ -43,6 +50,118 @@ public class Roll
             result[i] = arg.get(index);
         }
 
+        return result;
+    }
+
+    public static String roll(String[] args)
+    {
+        String[] filenames = {"legends.txt", "weapons.txt", "strats.txt", "weaponTypes.txt", "ammo.txt"};
+        Map<String, Vector<String>> data = new TreeMap<String, Vector<String>>();
+        String result = "";
+
+        for(String filename:filenames)
+        {
+            try
+            {
+                File file = new File(filename);
+                String str;
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+
+                data.put(filename, new Vector<String>());
+                
+                while((str = reader.readLine()) != null)
+                    data.get(filename).add(str);
+                
+                reader.close();
+            }
+            catch(FileNotFoundException e)
+            {
+                result = e.getMessage();
+                return result;
+            }
+            catch(IOException e)
+            {
+                result = e.getMessage();
+                return result;
+            }
+        }
+
+        List<String> argsAsList = Arrays.asList(args);
+
+        Boolean flagWeapons, flagLegends, flagStrat;
+        int personCount = 3;
+        weaponRollMode weaponRoll = weaponRollMode.WEAPONS;
+
+        //ARguments modify the default behaviour
+        flagWeapons = !argsAsList.contains("noweapons");
+        flagLegends = !argsAsList.contains("nolegends");
+        flagStrat = !argsAsList.contains("nostrat");
+        
+        if(argsAsList.contains("1"))
+            personCount = 1;
+        else if(argsAsList.contains("2"))
+            personCount = 2;
+        else if(argsAsList.contains("3"))
+            personCount = 3;
+        else if(argsAsList.contains("4"))
+            personCount = 4;
+
+        if(flagWeapons)
+        {
+            if(argsAsList.contains("weapons"))
+                weaponRoll = weaponRollMode.WEAPONS;
+            else if(argsAsList.contains("ammo"))
+                weaponRoll = weaponRollMode.AMMO;
+            else if(argsAsList.contains("types"))
+                weaponRoll = weaponRollMode.TYPES;
+        }
+
+        if(!(flagLegends || flagWeapons || flagStrat))
+        {
+            result = "ERROR: No rolls selected.";
+            return result;
+        }
+
+        String[][] results = new String[personCount + 1][2];
+
+        if(flagLegends)
+        {
+            String[] roll = manyUnique(data.get("legends.txt"), personCount);
+            for(int i = 0; i < personCount; i++)
+                results[i][0] = roll[i];
+        }
+
+        if(flagWeapons)
+        {
+            for(String[] tab:results)
+            {
+                String[] roll = {"", ""};
+                if(weaponRollMode.valueOf("WEAPONS") == weaponRoll)
+                    roll = manyUnique(data.get("weapons.txt"), 2);
+                else if(weaponRollMode.valueOf("TYPES") == weaponRoll)
+                    roll = many(data.get("weaponTypes.txt"), 2);
+                else if(weaponRollMode.valueOf("AMMO") == weaponRoll)
+                    roll = many(data.get("ammo.txt"), 2);
+                tab[1] = roll[0] + ", " + roll[1];
+            }
+        }
+
+        if(flagStrat)
+        {
+            results[results.length - 1][0] = one(data.get("strats.txt"));
+        }
+
+        for(int i = 0; i < results.length - 1; i++)
+        {
+            if(results.length != 1)
+            result += i + 1 + ".\n";
+            result += "Legend: " + (flagLegends ? results[i][0] : "Any") + "\n";
+            result += "Weapons: " + (flagWeapons ? results[i][1] : "Any") + "\n";
+            if(i != results.length - 2)
+                result += "\n";
+        }
+        result += "\nStrat: " + (flagStrat ? results[results.length - 1][0] : "Any") + "\n";
+        
         return result;
     }
 }
--- StratRoulette.java
+++ StratRoulette.java
@@ -1,121 +1,153 @@
-import java.io.*;
-import java.util.Vector;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
-import javax.lang.model.util.ElementScanner6;
-
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.List;
-import java.util.Arrays;
-
-public class StratRoulette
+public class StratRoulette extends Application
 {
-    enum weaponRollMode {AMMO, TYPES, WEAPONS};
+    @Override
+    public void start(Stage primaryStage)
+    {
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        root.setPadding(new Insets(25, 25, 25, 25));
+
+        // Text sceneTitle = new Text("Welcome!");
+        // sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        // root.add(sceneTitle, 0, 0, 2, 1);
+
+        // Label userName = new Label("Username:");
+        // root.add(userName, 0, 1);
+
+        // Label password = new Label("Password:");
+        // root.add(password, 0, 2);
+
+        // TextField userNameField = new TextField();
+        // root.add(userNameField, 1, 1);
+
+        // PasswordField passwordField = new PasswordField();
+        // root.add(passwordField, 1, 2);
+
+        // final Text actionTarget = new Text();
+        // root.add(actionTarget, 1, 6);
+
+        // Button btn = new Button("Sign in");
+        // HBox hbBtn = new HBox(10);
+        // hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        // hbBtn.getChildren().add(btn);
+        // btn.setOnAction(new EventHandler<ActionEvent>()
+        // {
+        //     @Override
+        //     public void handle(ActionEvent e)
+        //     {
+        //         actionTarget.setFill(Color.FIREBRICK);
+        //         actionTarget.setText("Sign in button pressed!");
+        //     }
+        // });
+        // root.add(hbBtn, 1, 4);
+
+        //TEAM SIZE CHOICE
+        Text teamSizeTitle = new Text("Team size:");
+
+        ChoiceBox teamSizeChoiceBox = new ChoiceBox();
+        teamSizeChoiceBox.getItems().addAll("1", "2", "3", "4");
+        teamSizeChoiceBox.setValue("3");
+
+        //LEGENDS CHOICE
+        Text legendsTitle = new Text("Legends:");
+
+        ChoiceBox legendsChoiceBox = new ChoiceBox();
+        legendsChoiceBox.getItems().addAll("Yes", "No");
+        legendsChoiceBox.setValue("Yes");
+
+        //WEAPONS CHOICE
+        Text weaponsTitle = new Text("Weapons:");
+
+        ChoiceBox weaponsChoiceBox = new ChoiceBox();
+        weaponsChoiceBox.getItems().addAll("Weapons", "Types", "Ammo", "No");
+        weaponsChoiceBox.setValue("Weapons");
+
+        //STRAT RADIOS
+        Text stratTitle = new Text("Strat:");
+
+        ChoiceBox stratsChoiceBox = new ChoiceBox();
+        stratsChoiceBox.getItems().addAll("Yes", "No");
+        stratsChoiceBox.setValue("Yes");
+
+        //ROLL BUTTON
+        Button rollButton = new Button("Roll!");
+        HBox rollButtonHBox = new HBox(10);
+        rollButtonHBox.getChildren().add(rollButton);
+        rollButtonHBox.setAlignment(Pos.CENTER);
+
+        //RESULTS TEXT
+        final Text resultsText = new Text();
+        VBox resultsTextVBox = new VBox(10);
+        resultsTextVBox.getChildren().add(resultsText);
+        resultsTextVBox.setMinSize(220, 300);
+        resultsTextVBox.setPadding(new Insets(0, 10, 0, 10));
+
+        //HANDLE ROLL EVENT
+        rollButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent e)
+            {
+                String[] args = {"3", "legends", "weapons", "strat"};
+                args[0] = (String)teamSizeChoiceBox.getValue();
+                args[1] = legendsChoiceBox.getValue().equals("Yes") ? "legends" : "nolegends";
+                
+                String arg2 = "weapons";
+                if(weaponsChoiceBox.getValue().equals("Weapons"))
+                    arg2 = "weapons";
+                else if(weaponsChoiceBox.getValue().equals("Types"))
+                    arg2 = "types";
+                else if(weaponsChoiceBox.getValue().equals("Ammo"))
+                    arg2 = "ammo";
+                else if(weaponsChoiceBox.getValue().equals("No"))
+                    arg2 = "noweapons";
+                args[2] = arg2;
+
+                args[3] = stratsChoiceBox.getValue().equals("Yes") ? "strat" : "nostrat";
+
+                String results = Roll.roll(args);
+                resultsText.setText(results);
+            }
+        });
+
+        //ADD EVERYTHING TO ROOT
+        root.add(teamSizeTitle, 0, 1);
+        root.add(teamSizeChoiceBox, 1, 1);
+        root.add(legendsTitle, 0, 2);
+        root.add(legendsChoiceBox, 1, 2);
+        root.add(weaponsTitle, 0, 3);
+        root.add(weaponsChoiceBox, 1, 3);
+        root.add(stratTitle, 0, 4);
+        root.add(stratsChoiceBox, 1, 4);
+        root.add(rollButtonHBox, 0, 5, 2, 1);
+        root.add(resultsTextVBox, 2, 0, 1, 5);
+
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Apex Legends Strat Roulette");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args)
     {
-        String[] filenames = {"legends.txt", "weapons.txt", "strats.txt", "weaponTypes.txt", "ammo.txt"};
-        Map<String, Vector<String>> data = new TreeMap<String, Vector<String>>();
-
-        for(String filename:filenames)
-        {
-            try
-            {
-                File file = new File(filename);
-                String str;
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-
-                data.put(filename, new Vector<String>());
-                
-                while((str = reader.readLine()) != null)
-                    data.get(filename).add(str);
-                
-                reader.close();
-            }
-            catch(FileNotFoundException e)
-            {
-                System.out.println(e);
-                return;
-            }
-            catch(IOException e)
-            {
-                System.out.println(e);
-                return;
-            }
-        }
-
-        List<String> argsAsList = Arrays.asList(args);
-
-        Boolean flagWeapons = true, flagLegends = true, flagStrat = true;
-        int personCount = 3;
-        weaponRollMode weaponRoll = weaponRollMode.WEAPONS;
-
-        //ARguments modify the default behaviour
-        if(args.length != 0)
-        {
-            flagWeapons = !argsAsList.contains("noweapons");
-            flagLegends = !argsAsList.contains("nolegends");
-            flagStrat = !argsAsList.contains("nostrat");
-            
-            if(argsAsList.contains("solo"))
-                personCount = 1;
-            else if(argsAsList.contains("duo"))
-                personCount = 2;
-            else if(argsAsList.contains("quad"))
-                personCount = 4;
-
-            if(flagWeapons)
-            {
-                if(argsAsList.contains("ammo"))
-                    weaponRoll = weaponRollMode.AMMO;
-                else if(argsAsList.contains("types"))
-                    weaponRoll = weaponRollMode.TYPES;
-            }
-        }
-
-        if(!(flagLegends || flagWeapons || flagStrat))
-        {
-            System.out.println("ERROR: No rolls selected.");
-            return;
-        }
-
-        String[][] results = new String[personCount + 1][2];
-
-        if(flagLegends)
-        {
-            String[] roll = Roll.manyUnique(data.get("legends.txt"), personCount);
-            for(int i = 0; i < personCount; i++)
-                results[i][0] = roll[i];
-        }
-
-        if(flagWeapons)
-        {
-            for(String[] tab:results)
-            {
-                String[] roll;
-                if(weaponRollMode.valueOf("WEAPONS") == weaponRoll)
-                    roll = Roll.manyUnique(data.get("weapons.txt"), 2);
-                else if(weaponRollMode.valueOf("AMMO") == weaponRoll)
-                    roll = Roll.many(data.get("ammo.txt"), 2);
-                else
-                    roll = Roll.many(data.get("weaponTypes.txt"), 2);
-                tab[1] = roll[0] + ", " + roll[1];
-            }
-        }
-
-        if(flagStrat)
-        {
-            results[results.length - 1][0] = Roll.one(data.get("strats.txt"));
-        }
-
-        for(int i = 0; i < results.length - 1; i++)
-        {
-            if(results.length != 1)
-                System.out.println(i + 1 + ".");
-            System.out.println("Legend: " + (flagLegends ? results[i][0] : "Any"));
-            System.out.println("Weapons: " + (flagWeapons ? results[i][1] : "Any"));
-            if(i != results.length - 2)
-                System.out.println("");
-        }
-        System.out.println("\nStrat: " + (flagStrat ? results[results.length - 1][0] : "Any"));
+        launch(args);
     }
 }
